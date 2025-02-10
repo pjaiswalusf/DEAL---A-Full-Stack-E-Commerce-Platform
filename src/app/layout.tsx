e94b814d242e50b997577175fd9ecc49c748161d
Pratik Jaiswal <pjaiswal@usf.edu>
2025-02-10 11:47:44 -0500

    FIxes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} antialiased bg-white`}>
+        className={`${inter.className} antialiased bg-white min-h-[125vh]`}>
           <Header />
         {children}
       </body>
