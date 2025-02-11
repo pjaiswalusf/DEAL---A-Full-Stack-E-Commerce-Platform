2ac8e4070ea32b622024329c3a7d4bb4fe2fe589
Pratik Jaiswal <pjaiswal@usf.edu>
2025-02-11 10:00:07 -0500

    Auth added

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -2,11 +2,13 @@
 
 import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/encoding";
 import { sha256 } from "@oslojs/crypto/sha2";
-
 import type { User, Session } from "@prisma/client";
 import prisma from "@/lib/prisma";
+import { cookies } from "next/headers";
+import { cache } from "react";
+import { error } from "console";
 
-export function generateSessionToken(): string {
+export async function generateSessionToken(): Promise<string> {
 	const bytes = new Uint8Array(20);
 	crypto.getRandomValues(bytes);
 	const token = encodeBase32LowerCaseNoPadding(bytes);
@@ -65,3 +67,127 @@ export async function invalidateSession(sessionId: string): Promise<void> {
 export type SessionValidationResult =
 	| { session: Session; user: User }
 	| { session: null; user: null };
+
+
+
+/* Cookies */
+
+export async function setSessionTokenCookie(token: string, expiresAt: Date): Promise<void> {
+	const cookieStore = await cookies();
+	cookieStore.set("session", token, {
+		httpOnly: true,
+		sameSite: "lax",
+		secure: process.env.NODE_ENV === "production",
+		expires: expiresAt,
+		path: "/"
+	});
+}
+
+export async function deleteSessionTokenCookie(): Promise<void> {
+	const cookieStore = await cookies();
+	cookieStore.set("session", "", {
+		httpOnly: true,
+		sameSite: "lax",
+		secure: process.env.NODE_ENV === "production",
+		maxAge: 0,
+		path: "/"
+	});
+}
+
+
+export const getCurrentSession = cache(async (): Promise<SessionValidationResult> => {
+	const cookieStore = await cookies();
+	const token = cookieStore.get("session")?.value ?? null;
+	if (token === null) {
+		return { session: null, user: null };
+	}
+	const result = await validateSessionToken(token);
+	return result;
+});
+
+/* User register, login, logout */
+
+export const hashPassword = async (password: string) => {
+	return encodeHexLowerCase(sha256(new TextEncoder().encode(password)));
+}
+
+export const verifyPassword = async(password: string, hash: string) => {
+	const passwordHash = await hashPassword(password);
+	return passwordHash === hash;
+}
+
+export const registerUser = async(email: string, password: string) => {
+	const passwordHash = await hashPassword(password);
+
+	try{
+		const user = await prisma.user.create({
+			data: {
+				email,
+				passwordHash
+			}
+		});
+
+		const safeUser = {
+			...user,
+			passwordHash: undefined,
+		};		
+
+		return {
+			user : safeUser,
+			error: null,
+		}
+
+	} catch(e){
+		return {
+			user: null,
+			error: "Failed to register user"
+		}
+	}
+}
+
+
+export const loginUser = async (email: string, password: string) => {
+	const user = await prisma.user.findUnique({
+		where: {
+			email: email,
+		}
+	});
+
+	if (!user){
+		return {
+			user: null,
+			error: "User not found",
+		}
+	}
+
+	const passwordValid = await verifyPassword(password, user.hashPassword);
+
+	if (!passwordValid){
+		return {
+			user: null,
+			error: "Invalid Password!",
+		}
+	}
+
+	const token = await generateSessionToken();
+	const session = await createSession(token, user.id);
+	await setSessionTokenCookie(token, session.expiresAt);
+
+	const safeUser = {
+		...user,
+		passwordHash: undefined,
+	};
+
+	return {
+		user: safeUser,
+		error: null,
+	}
+}
+
+export const logoutUser = async() => {
+	const session = await getCurrentSession();
+	if (session.session?.id){
+		await invalidateSession(session.session.id);
+	}
+	await deleteSessionTokenCookie();
+}
