f1f9ac849e35e02a9f45e6307e53cfa4f9b62eed
Pratik Jaiswal <pjaiswal@usf.edu>
2025-02-12 11:10:39 -0500

    Loaded data in the schemas

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,15 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { promotionCampaign } from './schemas/promotion-campaign'
+import { promotionCode } from './schemas/promotion-codes'
+import { product } from './schemas/product'
+import { productCategory } from './schemas/product-category'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    promotionCampaign,
+    promotionCode,
+    
+    product,
+    productCategory
+  ],
 }
