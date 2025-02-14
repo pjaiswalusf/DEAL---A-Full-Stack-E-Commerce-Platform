8686c58cd45725cb599597b4eb41982d11243e70
Pratik Jaiswal <pjaiswal@usf.edu>
2025-02-13 21:21:35 -0500

    Made addtions to the prisma

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true, // Disables ESLint checks in production builds
-  },
   images: {
     domains: ['cdn.sanity.io']
   }
