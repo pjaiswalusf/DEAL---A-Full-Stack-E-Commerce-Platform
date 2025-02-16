138a61bddd24aae593b9f600d9b8dda70d4f47a8
Pratik Jaiswal <pjaiswal@usf.edu>
2025-02-16 14:16:05 -0500

    Installed shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,6 @@
-// import { clsx, type ClassValue } from "clsx"
-// import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-// export function cn(...inputs: ClassValue[]) {
-//   return twMerge(clsx(inputs))
-// }
-
-export const formatPrice = (price: number) => {
-  return new Intl.NumberFormat("en-US", {
-      style: 'currency',
-      currency: 'USD',
-  }).format(price);
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
